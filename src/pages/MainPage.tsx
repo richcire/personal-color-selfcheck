1e1655b6204b1fc9a18b0a635ec2652057383f92
richcire <eric9907@naver.com>
2022-06-14 21:58:35 +0900

    initial commit

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,7 @@
+import "../index.css";
+
+function MainPage() {
+  return null;
+}
+
+export default MainPage;
---
+++ src/pages/StartPage.tsx
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  margin-top: 80px;
+  font-size: 8em;
+`;
+
+const StartBtn = styled.button`
+  margin-top: 300px;
+  height: 130px;
+  width: 250px;
+  border-radius: 10px;
+  font-size: 3em;
+`;
+
+function StartPage() {
+  const navigate = useNavigate();
+  const onStartBtnClicked = () => {
+    navigate("/main");
+  };
+
+  return (
+    <Container>
+      <Title>퍼스널 컬러 자가진단</Title>
+      <StartBtn onClick={onStartBtnClicked}>시작하기</StartBtn>
+    </Container>
+  );
+}
+
+export default StartPage;
