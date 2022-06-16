9ba26f1a669198597407030a142e6cce49fecfff
richcire <eric9907@naver.com>
2022-06-16 21:52:29 +0900

    get parameter from url

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,20 +1,55 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import CardContainer from "../components/CardContainer";
 
-const Intro = styled.h2``;
-const Result = styled.div``;
-const Explanation = styled.div``;
+const Intro = styled.h2`
+  padding-top: 20px;
+  display: flex;
+  align-items: center;
+  margin: 0;
+  font-size: 2rem;
+  height: 8%;
+`;
+
+const Result = styled.div`
+  font-size: 5rem;
+  height: 12%;
+`;
+
+const Explanation = styled.div`
+  background-color: whitesmoke;
+  width: 50%;
+  font-size: 1.2rem;
+  border-radius: 20px;
+  height: 20%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: scroll;
+`;
 
 const Card = styled.div``;
 
 function ResultPage() {
-  const { result } = useParams();
+  const { id } = useParams();
+
+  const [koreanResult, setKoreanResult] = useState("봄 웜");
+
+  useEffect(() => {
+    if (id === "summerCool") {
+      setKoreanResult("여름 쿨");
+    } else if (id === "fallWarm") {
+      setKoreanResult("가을 웜");
+    } else if (id === "winterCool") {
+      setKoreanResult("겨울 쿨");
+    }
+  }, []);
 
   return (
     <>
       <Intro>당신의 퍼스널 컬러 결과는...</Intro>
-      <Result>봄 웜</Result>
+      <Result>{koreanResult}</Result>
       <Explanation>explanation</Explanation>
 
       <CardContainer></CardContainer>
