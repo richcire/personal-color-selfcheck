b41ad79027749b90aa3f231400f054f4a3114c0a
richcire <eric9907@naver.com>
2022-06-17 15:57:58 +0900

    show explanation depeding on result

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -2,6 +2,12 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import CardContainer from "../components/CardContainer";
+import {
+  fallWarmData,
+  springWarmData,
+  summerCoolData,
+  winterCoolData,
+} from "../ResultData";
 
 const Intro = styled.h2`
   padding-top: 20px;
@@ -34,23 +40,28 @@ const Card = styled.div``;
 function ResultPage() {
   const { id } = useParams();
 
-  const [koreanResult, setKoreanResult] = useState("봄 웜");
+  let koreanResult = "봄 웜";
+  let explanation = "";
 
-  useEffect(() => {
-    if (id === "summerCool") {
-      setKoreanResult("여름 쿨");
-    } else if (id === "fallWarm") {
-      setKoreanResult("가을 웜");
-    } else if (id === "winterCool") {
-      setKoreanResult("겨울 쿨");
-    }
-  }, []);
+  if (id === "springWarm") {
+    koreanResult = "봄 웜";
+    explanation = springWarmData.explanation;
+  } else if (id === "summerCool") {
+    koreanResult = "여름 쿨";
+    explanation = summerCoolData.explanation;
+  } else if (id === "fallWarm") {
+    koreanResult = "가을 웜";
+    explanation = fallWarmData.explanation;
+  } else {
+    koreanResult = "겨울 쿨";
+    explanation = winterCoolData.explanation;
+  }
 
   return (
     <>
       <Intro>당신의 퍼스널 컬러 결과는...</Intro>
       <Result>{koreanResult}</Result>
-      <Explanation>explanation</Explanation>
+      <Explanation>{explanation}</Explanation>
 
       <CardContainer></CardContainer>
     </>
